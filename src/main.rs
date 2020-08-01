dd9f1291063b40e1b17d8abbc451a0c49071c9da
dgarrets <dgarrets@trinity.edu>
2020-08-01 17:36:46 -0500

    Playing with some intentionally returned statuses

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,31 @@ use rocket::data::Data;
 use rocket_contrib::json::JsonValue;
 use serde_json::Value;
 use std::io::Read;
+use csv::Reader;
+
+use serde::Serialize;
+use rocket_contrib::json::Json;
+use rocket::http::Status;
+
+#[derive(Serialize)]
+struct Task { a: i32 }
+
+#[post("/test/<number>")]
+fn test_custom_response(number: i32) -> Result<Json<Task>, Status>{ 
+    if number != 0 {
+        Ok(Json(Task{ 
+            a: number
+        }))
+    } else {
+        Err(Status::NotAcceptable)
+    }
+}
 
 #[post("/submit", data = "<data>")]
 fn convert_csv_to_json(data: Data) -> Result<JsonValue, Box<dyn std::error::Error>> {
     let mut stream = data.open();
     let mut csv_stream = String::new();
-    stream.read_to_string(&mut csv_stream)?;
-
-    println!("{}", &csv_stream);
+    stream.read_to_string(&mut csv_stream);
 
     let mut csv_trimmed = String::new();
 
@@ -40,8 +57,8 @@ fn convert_csv_to_json(data: Data) -> Result<JsonValue, Box<dyn std::error::Erro
         }
     }
 
-    let mut rdr1 = csv::Reader::from_reader(csv_trimmed.as_bytes());
-    let mut rdr2 = csv::Reader::from_reader(csv_trimmed.as_bytes());
+    let mut rdr1 = Reader::from_reader(csv_trimmed.as_bytes());
+    let mut rdr2 = Reader::from_reader(csv_trimmed.as_bytes());
 
     let mut json_string = String::new();
 
@@ -82,6 +99,7 @@ fn main() {
         .mount(
             "/",
             routes![
+                test_custom_response,
                 convert_csv_to_json
             ],
         )
