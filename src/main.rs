813523ed9eaf7d74b9a24ffd040b4d30eb54b45a
dgarrets <dgarrets@trinity.edu>
2020-08-01 22:46:04 -0500

    Updating documentation, comments, and some variable names

--- src/main.rs
+++ src/main.rs
@@ -12,94 +12,108 @@ use rocket_contrib::json::JsonValue;
 use serde_json::Value;
 use std::io::Read;
 use csv::Reader;
-
-use serde::Serialize;
-use rocket_contrib::json::Json;
 use rocket::http::Status;
 
-#[derive(Serialize)]
-struct Task { a: i32 }
-
-#[post("/test/<number>")]
-fn test_custom_response(number: i32) -> Result<Json<Task>, Status>{ 
-    if number != 0 {
-        Ok(Json(Task{ 
-            a: number
-        }))
-    } else {
-        Err(Status::NotAcceptable)
-    }
-}
-
+///Takes a CSV from a POST request and responds with a list of JSON Objects constructed from those
+///records
+///
+///# Arguments
+///
+///* `data` - The raw form-data passed in from the post request, retrieved by Rocket
 #[post("/submit", data = "<data>")]
-fn convert_csv_to_json(data: Data) -> Result<JsonValue, Box<dyn std::error::Error>> {
+fn convert_csv_to_json(data: Data) -> Result<JsonValue, Status> {
     let mut stream = data.open();
     let mut csv_stream = String::new();
     stream.read_to_string(&mut csv_stream);
 
+    let mut csv_id = String::new();
     let mut csv_trimmed = String::new();
 
-    let mut csv_id = String::new();
-
     for (count, c) in csv_stream.lines().enumerate() {
-
+        //Storing the first line of the Rocket DataStream generated from the input file
+        //This line is comprised of hyphens and an identifier
         if count == 0 {
             csv_id.push_str(&c);
         }
 
-        if (c.contains(&csv_id) || c == "")  && count != 3 && count != 0 {
-            break;
+        //Throwing an error if the submitted file is not a csv
+        if count == 2 && c != "Content-Type: text/csv" {
+            return Err(Status::UnprocessableEntity);
         }
 
+        //As the first four lines of the DataStream do not contain csv records, they are ignored
         if count > 3 {
-            csv_trimmed.push_str(c);
-            csv_trimmed.push('\n');
+            //If we reach an empty line or a line matching the DataStream identifier, we have
+            //reached the end of readable csv records. Otherwise, we save the line
+            if c.contains(&csv_id) || c == "" {
+                break;
+            } else {
+                csv_trimmed.push_str(c);
+                csv_trimmed.push('\n');
+            }
         }
     }
 
+    //Creating two readers so we can iterate through Reader.headers() and Reader.records()
+    //simultaneously without borrowing conflicts
     let mut rdr1 = Reader::from_reader(csv_trimmed.as_bytes());
     let mut rdr2 = Reader::from_reader(csv_trimmed.as_bytes());
 
+    //Transforming the data from a CSV string to a JSON style string
     let mut json_string = String::new();
 
     json_string.push_str("[");
 
-    let headers = rdr1.headers()?;
+    let headers = match rdr1.headers() {
+        Ok(csv_headers) => csv_headers,
+        Err(_csv_headers) => return Err(Status::UnprocessableEntity)
+    };
 
     for result in rdr2.records() {
-        let record = result?;
+        let record = match result{
+            Ok(v) => v,
+            Err(_v) => return Err(Status::UnprocessableEntity)
+        };
 
         json_string.push_str("{\n");
 
-        let special_iter = headers.iter().zip(record.iter());
+        //Zipping the headers and records together so as we iterate through the records, we can
+        //concatenate the matching header easily
+        let zipped_iter = headers.iter().zip(record.iter());
 
-        for s in special_iter {
-            json_string.push_str(&format!("\t\"{}\": \"{}\",\n", s.0, s.1));
+        for z in zipped_iter {
+            json_string.push_str(&format!("\t\"{}\": \"{}\",\n", z.0, z.1));
         }
 
+        //Popping twice to remove the trailing comma on the last field
         json_string.pop();
         json_string.pop();
-        json_string.push('\n');
-
-        json_string.push_str("},\n");
+        json_string.push_str("\n},\n");
     }
 
+    //Popping twice to remove the trailing comma on the last object
     json_string.pop();
     json_string.pop();
     json_string.push_str("]\n");
 
-    let v: Value = serde_json::from_str(&json_string)?;
-    let j = json!(v);
+    //Converting from String to a serde_json Value
+    let v: Value = match serde_json::from_str(&json_string) {
+        Ok(value) => value,
+        Err(_value) => return Err(Status::InternalServerError)
+    };
 
-    Ok(j)
+    //Converting from a serde_json Value to a rocket JsonValue
+    let rocket_json_value = json!(v);
+
+    Ok(rocket_json_value)
 }
 
+///Calls the ignite function from Rocket to start the server
 fn main() {
     rocket::ignite()
         .mount(
             "/",
             routes![
-                test_custom_response,
                 convert_csv_to_json
             ],
         )
